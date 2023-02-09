64eba5c842b76a8e59e7610f30a05c904745a390
Szymon1948 <szymonintrys@gmail.com>
2023-02-09 10:15:06 +0100

    Recycle View -ProductViewHolder

--- app/src/main/java/com/example/listazakupow_recycleview/MainActivity.java
+++ app/src/main/java/com/example/listazakupow_recycleview/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.LinkedList;
 
+public class MainActivity extends AppCompatActivity {
+    LinkedList <String> produkty = new LinkedList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wypelnijListe();
+    }
+
+    private void wypelnijListe(){
+        produkty.addLast("jablka");
+        produkty.addLast("ciasto");
+        produkty.addLast("pepsi");
+        produkty.addLast("jaja");
+        produkty.addLast("chleb");
+        produkty.addLast("kielbasa");
+        produkty.addLast("nudle");
+        produkty.addLast("ser");
+        produkty.addLast("gorzka_czekolada");
     }
 }
--- app/src/main/java/com/example/listazakupow_recycleview/ShoppingAdapter.java
+++ app/src/main/java/com/example/listazakupow_recycleview/ShoppingAdapter.java
@@ -1,2 +1,44 @@
-package com.example.listazakupow_recycleview;public class ShoppingAdapter {
+package com.example.listazakupow_recycleview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+//3-Implement Methods
+public class ShoppingAdapter extends RecyclerView.Adapter<ShoppingAdapter.ProduktViewHolder> {
+
+    private LayoutInflater inflater;
+
+    @NonNull
+    @Override
+    public ProduktViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemView = inflater.inflate(R.layout.item_product, parent, false);
+        return new ProduktViewHolder(itemView,this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProduktViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    //1-Create class 'ProduktViewHolder'
+                                   //2-Create constructor matching super
+    public class ProduktViewHolder extends RecyclerView.ViewHolder {
+    public final TextView productItemView;
+    public final ShoppingAdapter shoppingAdapter;
+    public ProduktViewHolder(@NonNull View itemView, ShoppingAdapter adapter) {
+        super(itemView);
+        productItemView = itemView.findViewById(R.id.textView);
+        shoppingAdapter = adapter;
+    }
+}
 }
